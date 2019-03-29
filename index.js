934cb519e74cd01d2c633b33808b6dfd904a11b8
Ariel Barcellos <barcellos.ariel@gmail.com>
2019-03-29 17:21:18 -0500

    added comments

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const sayHello = () => console.log('Hello world')
-
-sayHello()
-
 /* 
 
 Write a function that takes two strings, s1 and s2 
@@ -16,16 +12,20 @@ and returns the longest common subsequence of s1 and s2
 const longestSubseq = (s1, s2, s1StartIdx = 0, s2StartIdx = 0) => {
   const results = []
 
+  // Here we will loop through s1 and try to find the s1 character on s2
   for (let s1Idx = s1StartIdx; s1Idx < s1.length; s1Idx++) {
     const s1Char = s1[s1Idx]
     const s2Idx = s2.indexOf(s1Char, s2StartIdx)
 
     const result = []
 
+    /* If it finds the character on s2, it will push it to result array and call
+    this function to the next position.
+    The result array will be filled with a subsequence candidate or
+    it will be empty */
     if (s2Idx !== -1) {
       result.push(s1Char, ...longestSubseq(s1, s2, s1Idx + 1, s2Idx + 1))
     }
-
     results.push(result)
   }
   
@@ -33,6 +33,8 @@ const longestSubseq = (s1, s2, s1StartIdx = 0, s2StartIdx = 0) => {
   return longest.join('')
 }
 
+/* Here it is really simple, it gets all the subsequences 
+and returns the longest one */
 const findLongest = (candidates) => {
   let longest = []
 
