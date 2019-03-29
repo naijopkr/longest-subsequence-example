e15b07928d48a83bdb16685c615db1be59e31175
Ariel Barcellos <barcellos.ariel@gmail.com>
2019-03-29 16:48:43 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const sayHello = () => console.log('Hello world')
+
+sayHello()
+
+/* 
+
+Write a function that takes two strings, s1 and s2 
+and returns the longest common subsequence of s1 and s2
+
+"ABAZDC", "BACBAD" => "ABAD"
+"AGGTAB", "GXTXAYB" => "GTAB"
+"aaaa", "aa" => "aa"
+
+*/
